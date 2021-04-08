e3fa987b3628903f2228558c7ee62e1a915031f2
Edventures <edufg13@gmail.com>
2021-04-08 02:58:34 +0000

    proceso v1

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Person, Parent
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,87 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/all', methods=['GET'])
+def get_person():    
+    return jsonify(Person.getAllPerson()), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+@app.route('/allp', methods=['GET'])
+def get_parent():    
+    return jsonify(Parent.getAllParent()), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    person = Person.getSpecificMember(id)
+    parents = Person.getAllParent(id)
+    #sons = Person.getAllSons(id)
+    familyTree = {
+        "person": person,
+        "parents": parents,
+        #"sons": sons
     }
+    return jsonify(familyTree), 200
 
-    return jsonify(response_body), 200
+@app.route('/relation', methods=['POST'])
+def handle_parent():
+    #----------------------------CREATE POST
+    if request.method == 'POST':
+        body = request.get_json()
+        if body is None:
+            return "The request body is null", 400
+        if 'name' not in body:
+            return "You need to specify the name", 400
+        if 'last_name' not in body:
+            return "You need to specify the last_name", 400
+        if 'age' not in body:
+            return "You need to specify the age", 400
+        if 'person_id' not in body:
+            return "You need to specify the person_id", 400
+        if 'type' not in body:
+            return "You need to specify the type", 400
+        
+        person = Person()
+        relative1 = Parent()
+        relative2 = Parent()
+
+        if body['type'] == 'father':
+            relative1.relativity = 'Father'
+            response_body = {"msg": "You POST a FATHER"}
+        elif body['type'] == 'mother':
+            relative1.relativity = 'Mother'
+            response_body = {"msg": "You POST a MOTHER"}
+        else:
+            return "You need to specify a correct type", 400
+        
+        person.name = body['name']
+        person.last_name = body['last_name']
+        person.age = body['age']
+
+        db.session.add(person)
+        db.session.commit()
+        
+        relative1.name = body['name']
+        relative1.last_name = body['last_name']
+        relative1.son_id = body['person_id']
+
+        fromPerson = Person.query.filter_by(id = body['person_id']).first()
+        relative2.relativity = 'Son/Daughter'
+        relative2.name = fromPerson.name
+        relative2.last_name = fromPerson.last_name
+        relative2.own_id = fromPerson.id
+
+        newPerson = Person.query.filter_by(name=body['name'], last_name=body['last_name'], age=body['age']).first()
+        if body['type'] == 'father':
+            relative2.father_id = newPerson.id
+        else:
+            relative2.mother_id = newPerson.id
+        
+        relative1.own_id = newPerson.id
+        
+        db.session.add(relative1)
+        db.session.add(relative2)
+        db.session.commit()        
+
+        return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,103 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Parent(db.Model):
+    __tablename__ = 'parent'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    son_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=True)
+    father_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=True)
+    mother_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=True)
+    own_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
+    relativity = db.Column(db.String(250), nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def getAllParent():
+        list_parent = Parent.query.all()
+        list_parent = list(map(lambda x: x.serialize(), list_parent))
+        return(list_parent) 
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "father_id": self.father_id,
+            "mother_id": self.mother_id,
+            "son_id": self.son_id,
+            "own_id": self.own_id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "relativity": self.relativity,            
+        }
+
+class Person(db.Model):
+    __tablename__ = 'person'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+    def getAllPerson():
+        list_person = Person.query.all()
+        list_person = list(map(lambda x: x.serialize(), list_person))
+        list_person.sort(key=lambda x: x["age"], reverse=True) 
+        return(list_person)
+
+    def getSpecificMember(id):
+        member = Person.query.filter_by(id=id).first()
+        person = {
+            "name": member.name,
+            "last_name": member.last_name,
+            "age": member.age,
+        }
+        return person
+
+    # def getAllParent(id):
+    #     member = Parent.query.filter_by(son_id=id).first()
+    #     if member is None:
+    #         return "No tiene padres"
+    #     father = Person.getSpecificMember(member.father_id)
+    #     mother = Person.getSpecificMember(member.mother_id)
+    #     response = {
+    #         "father": father,
+    #         #"mother": mother
+    #     }
+    #     return response
+
+    def getAllParent(id):
+        member1 = Parent.query.filter_by(son_id=id, relativity = 'Father').first()
+        member2 = Parent.query.filter_by(son_id=id, relativity = 'Mother').first()
+        if member1 is None and member2 is None:
+            return "There are no info of the parents"
+        elif member1 is None:
+            mother = Person.getSpecificMember(member2.own_id)
+            response = {"father":"None", "mother": mother}
+        elif member2 is None:
+            father = Person.getSpecificMember(member1.own_id)
+            response = {"father": father, "mother": "None"}
+        else:
+            father = Person.getSpecificMember(member1.own_id)
+            mother = Person.getSpecificMember(member2.own_id)
+            response = {"father": father, "mother": mother}
+        return response
+
+    # def getAllSons(id):
+    #     member_list = Parent.query.filter(or_(Parent.father_id==id, Parent.mother_id==id))
+    #     if member_list is None:
+    #         return "No tiene hijos"
+    #     member_list = list(map(lambda x: Person.getSpecificMember(x.son_id), member_list))
+    #     if len(member_list) == 0:
+    #         return "No tiene hijos"
+    #     response = {
+    #         "sons": member_list
+    #     }
+    #     return response
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "age": self.age,
         }
